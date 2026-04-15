c4abfa95ad947a843de6528f42a8e00c8f886ec3
Kambin Zarei <87777965+Kambin-Zarei@users.noreply.github.com>
2026-04-15 15:23:46 -0400

    feat: add WorkoutTemplate, WorkoutLog, Equipment, and RecCenterRoom models with migrations

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,2 +1,6 @@
 from .db import db
+from .equipment import Equipment
 from .user import User
+from .rec_center_room import RecCenterRoom
+from .workout_log import WorkoutLog
+from .workout_template import WorkoutTemplate
